ddc5beecb711f64d9e8bb1a718ac87be5d76700b
Ygee <ygee@yearz.co>
2021-02-08 11:43:19 +0800

    Added BlurView

---
+++ Sources/SwiftPro/UIKit/BlurView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+@available(iOS 13, *)
+public struct BlurView: UIViewRepresentable {
+    public typealias UIViewType = UIView
+    public var style: UIBlurEffect.Style
+
+    public func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: CGRect.zero)
+        view.backgroundColor = .clear
+
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+
+        NSLayoutConstraint.activate([
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            ])
+
+        return view
+    }
+
+    public func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<BlurView>) {
+    }
+}
