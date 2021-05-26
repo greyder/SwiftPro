b1890998c978cfe9d2147cad75d15c4e4ed67d87
ygee <ygee@yearz.co>
2021-05-26 10:17:18 +0800

    Added Email and Password Validator

--- Sources/Extensions/StringExtension.swift
+++ Sources/Extensions/StringExtension.swift
@@ -1,7 +1,21 @@
+import Foundation
+
 extension String {
     
     /// A Boolean value indicating whether the string only contains whitespace
     public var isBlank: Bool {
         return allSatisfy({ $0.isWhitespace })
+    }
+    
+    /// A Boolean value indicating whether the string is a valid email
+    public var isValidEmail: Bool {
+        return NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
+    }
+    
+    
+    /// A Boolean value indicating whether the string is a strong password with at least 1 lowercase, 1 uppercase, 1 digit, 1 special character, and 8 characters in length
+    public func isStrongPassword(minLength: Int = 8) -> Bool {
+        let regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[d$@$!%*?&#.])[A-Za-z\\dd$@$!%*?&#.]{\(minLength),}"
+        return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: self)
     }
 }
--- Tests/StringTests/StringTests.swift
+++ Tests/StringTests/StringTests.swift
@@ -1,15 +1,10 @@
 import XCTest
 //@testable import StringExtension
 //@testable provides access only for internal functions; fileprivate and private declarations are not visible outside of their usual scope when using testable.
+//since we are not testing internal functions and only public facing ones, we don't need to use testable.
 import StringExtension
 
 final class StringTests: XCTestCase {
-    
-    static var allTests = [
-        ("testIsBlankIfEmpty", testIsBlankIfEmpty),
-        ("testIsBlankIfWhitespace", testIsBlankIfWhitespace),
-        ("testIsBlankIfPresent", testIsBlankIfPresent)
-    ]
     
     /// Equivalent to isEmpty
     func testIsBlankIfEmpty() {
@@ -25,5 +20,50 @@ final class StringTests: XCTestCase {
     func testIsBlankIfPresent() {
         let string = "abc"
         XCTAssertFalse(string.isBlank)
+    }
+    
+    func testEmailIsInvalid() {
+        let string = "email@com"
+        XCTAssertFalse(string.isValidEmail)
+    }
+    
+    func testEmailIsBlank() {
+        let string = "  "
+        XCTAssertFalse(string.isValidEmail)
+    }
+    
+    func testEmailIsValid() {
+        let string = "email@mail.com"
+        XCTAssertTrue(string.isValidEmail)
+    }
+    
+    func testPasswordIsWeak() {
+        let string = "abcd1234"
+        XCTAssertFalse(string.isStrongPassword())
+    }
+    
+    func testPasswordIsBlank() {
+        let string = "  "
+        XCTAssertFalse(string.isStrongPassword())
+    }
+    
+    func testPasswordIsShort() {
+        let string = "Pw123!"
+        XCTAssertFalse(string.isStrongPassword())
+    }
+    
+    func testPasswordIsLong() {
+        let string = "Password123!Password123!Password123!"
+        XCTAssertTrue(string.isStrongPassword())
+    }
+    
+    func testPasswordMinLength() {
+        let string = "Pw123!"
+        XCTAssertTrue(string.isStrongPassword(minLength: 4))
+    }
+    
+    func testPasswordIsStrong() {
+        let string = "Password123!"
+        XCTAssertTrue(string.isStrongPassword())
     }
 }
